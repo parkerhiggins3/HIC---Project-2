0ef6ab5964a0d32fbbbce9225a0ab3e0b1c8aadc
Alex Ely <ely.m.alexander@gmail.com>
2022-12-02 15:12:43 -0500

    Fixed and tested SQL files

--- Backend/SQL/create_sales.sql
+++ Backend/SQL/create_sales.sql
@@ -1,17 +1,19 @@
 CREATE TABLE customer (
-	user_id VARCHAR(99),
-	first_name VARCHAR(99) NOT NULL,
-	last_name VARCHAR(99) NOT NULL,
-	
-	address VARCHAR(99),
-	city VARCHAR(99),
-	state VARCHAR(99),
-	zip VARCHAR(99),
-	
-	email VARCHAR(99) NOT NULL,
+    user_id VARCHAR(99),
+    first_name VARCHAR(99) NOT NULL,
+    last_name VARCHAR(99) NOT NULL,
+    
+    street_number VARCHAR(99),
+    street_name VARCHAR(99),
+    street_apt VARCHAR(99),
+    city VARCHAR(99),
+    state VARCHAR(99),
+    zip VARCHAR(99),
+    
+    email VARCHAR(99) NOT NULL,
     phone_number VARCHAR(99) NOT NULL,
-	
-	PRIMARY KEY (user_id)
+    
+    PRIMARY KEY (user_id)
 );
 
 CREATE TABLE category(
@@ -27,42 +29,52 @@ CREATE TABLE payment(
     expiration_date DATE,
     
     first_name VARCHAR(99) NOT NULL,
-	last_name VARCHAR(99) NOT NULL,
+    last_name VARCHAR(99) NOT NULL,
     
     street_number VARCHAR(99),
-	street_name VARCHAR(99),
-	street_apt VARCHAR(99),
-	city VARCHAR(99),
-	state CHAR(99),
-	zip VARCHAR(99),
+    street_name VARCHAR(99),
+    street_apt VARCHAR(99),
+    city VARCHAR(99),
+    state CHAR(99),
+    zip VARCHAR(99),
 
     PRIMARY KEY(card_number)
 );
 
 CREATE TABLE product(
-	product_id VARCHAR(99),
-	product_name VARCHAR(99),
-	price INT,
+    product_id VARCHAR(99),
+    product_name VARCHAR(99),
+    price DECIMAL,
     quantity INT,
     category_id VARCHAR(99),
     size VARCHAR(99),
     color VARCHAR(99),
     material VARCHAR(99),
     product_description VARCHAR(99),
-	product_image VARCHAR(99),
-	
-	PRIMARY KEY (product_id),
+    product_image TEXT(65000),
+    
+
+    PRIMARY KEY (product_id),
     FOREIGN KEY(category_id) REFERENCES category(category_id)
 );
 
 CREATE TABLE orders(
     order_id VARCHAR(99),
-    subtotal INT,
-    taxes INT,
-    total INT,
+    user_id  VARCHAR(99),
+    subtotal DECIMAL,
+    taxes DECIMAL,
+    total DECIMAL,
     card_number VARCHAR(99),
     PRIMARY KEY(order_id),
-    FOREIGN KEY(card_number) REFERENCES payment(card_number)
+    FOREIGN KEY(user_id) REFERENCES customer(user_id)
+);
+
+CREATE TABLE product_to_order(
+    product_id VARCHAR(99),
+    order_id   VARCHAR(99),
+    PRIMARY KEY(product_id, order_id),
+    FOREIGN KEY(product_id) REFERENCES product(product_id),
+    FOREIGN KEY(order_id) REFERENCES orders(order_id)
 );
 
 CREATE TABLE shopping_cart(
@@ -71,8 +83,3 @@ CREATE TABLE shopping_cart(
        PRIMARY KEY(cart_id),
        FOREIGN KEY(product_id) REFERENCES product(product_id)
 );
-
-
-
-
-
--- Backend/SQL/insert_data.sql
+++ Backend/SQL/insert_data.sql
@@ -1,29 +1,27 @@
-
-
-INSERT INTO customer VALUES('01','NULL', 'John', 'Doe', '241','FDR ST','Apt 4f', 'New York City', 'NY', '11203', 'jdoe@aol.com','Password123','999-234-2131');
-INSERT INTO customer VALUES('02','NULL', 'Juan', 'Quando', '321','GWB ST','Apt 1f', 'Brooklyn', 'NY', '11235', 'jq@aol.com','Password345','919-224-2231');
-INSERT INTO customer VALUES('03','NULL', 'Villados', 'Cronos', '322','OHB ST','Apt 6f', 'kent', 'OH', '44232', 'vcron@aol.com','Password143','943-123-4531');
-
-INSERT INTO product VALUES('01-1', 'KT-Shirt', '7.99', '10','0', 'S','Black','Cotton','Quality made Shirt', 'NULL');
-INSERT INTO product VALUES('01-2', 'KT-Shirt', '7.99', '10','1', 'M','Yellow','Cotton','Quality made Shirt', 'NULL');
-INSERT INTO product VALUES('01-3', 'KT-Shirt', '7.99', '10','2', 'L','Blue','Cotton','Quality made Shirt', 'NULL');
-INSERT INTO product VALUES('01-4', 'KT-Shirt', '7.99', '10','3', 'XL','white','Cotton','Quality made Shirt', 'NULL');
-
-INSERT INTO product VALUES('02-1', 'K-Polo', '8.99', '10','0', 'M','Black','Silk','Quality made Polo', 'NULL');
-INSERT INTO product VALUES('02-2', 'K-Polo', '8.99', '10','1', 'S','white','Silk','Quality made Polo', 'NULL');
-INSERT INTO product VALUES('02-3', 'K-Polo', '8.99', '10','2', 'XL','Blue','Silk','Quality made Polo', 'NULL');
-INSERT INTO product VALUES('02-4', 'K-Polo', '8.99', '10','3', 'L','Yellow','Silk','Quality made Polo', 'NULL');
-
-INSERT INTO product VALUES('03-1', 'K-Jeans', '9.99', '10','0', 'XL','Black','Denim','Quality made jeans', 'NULL');
-INSERT INTO product VALUES('03-2', 'K-Jeans', '9.99', '10','1', 'L','Light Blue','Denim','Quality made jeans', 'NULL');
-INSERT INTO product VALUES('03-3', 'K-Jeans', '9.99', '10','2', 'M','Brown','Denim','Quality made jeans', 'NULL');
-INSERT INTO product VALUES('03-4', 'K-Pants', '9.99', '10','3', 'S','Dark Blue','Denim','Quality made jeans', 'NULL');
-
-INSERT INTO payment VALUES('678-232-1230', '420','01','2025-12-05','John', 'Doe', '241', 'FDR ST', 'Apt 4f', 'New York City', 'NY', '11203');
-INSERT INTO payment VALUES('678-222-1130', '410','02','2025-11-11','Juan', 'Quando', '321', 'GWB ST', 'Apt 1f', 'Brooklyn', 'NY', '11235');
-INSERT INTO payment VALUES('678-232-1120', '430','03','2025-12-20','Villados', 'Cronos', '322', 'OHB ST', 'Apt 6f', 'Kent', 'OH', '44232');
+INSERT INTO customer VALUES('01', 'John', 'Doe', '241','FDR ST','Apt 4f', 'New York City', 'NY', '11203', 'jdoe@aol.com','999-234-2131');
+INSERT INTO customer VALUES('02', 'Juan', 'Quando', '321','GWB ST','Apt 1f', 'Brooklyn', 'NY', '11235', 'jq@aol.com','919-224-2231');
+INSERT INTO customer VALUES('03', 'Villados', 'Cronos', '322','OHB ST','Apt 6f', 'kent', 'OH', '44232', 'vcron@aol.com','943-123-4531');
 
 INSERT INTO category VALUES('0', 'Girls', 'Girls Clothing');
 INSERT INTO category VALUES('1', 'Boys', 'Boys Clothing');
 INSERT INTO category VALUES('2', 'Women', 'Women Clothing');
 INSERT INTO category VALUES('3', 'Men', 'Mens Clothing');
+
+INSERT INTO product VALUES('011', 'T-Shirt', 7.99, '10','0', 'S','Black','Cotton','Quality made Shirt', 'https://www.rlmedia.io/is/image/PoloGSI/s7-1412708_lifestyle?$rl_df_zoom_lif$');
+INSERT INTO product VALUES('012', 'Dress Shirt', 7.99, '10','1', 'M','Yellow','Cotton','Quality made Shirt', 'https://image.menswearhouse.com/is/image/TMW/MW40_540K_01_CALVIN_KLEIN_WHITE_MAIN?$40GridDesktop2x$');
+INSERT INTO product VALUES('013', 'Short sleeve Shirt', 7.99, '10','2', 'L','Blue','Cotton','Quality made Shirt', 'https://m.media-amazon.com/images/I/71abnYEKqML._AC_UX679_.jpg');
+INSERT INTO product VALUES('014', 'Long Sleeved Shirt', 7.99, '10','3', 'XL','white','Cotton','Quality made Shirt', 'https://m.media-amazon.com/images/I/61lsK4whksL._AC_UX679_.jpg');
+
+INSERT INTO product VALUES('021', 'K-Polo', 8.99, '10','0', 'M','Black','Silk','Quality made Polo', 'https://m.media-amazon.com/images/I/7176fMiEuWL._AC_UX679_.jpg');
+INSERT INTO product VALUES('022', 'K-Socks', 4.99, '10','1', 'S','white','Silk','Quality made Socks', 'https://m.media-amazon.com/images/I/91bXFrnDWxL._AC_UX679_.jpg');
+INSERT INTO product VALUES('023', 'K-Hats', 1.99, '10','2', 'XL','Blue','Silk','Quality made Hats', 'https://m.media-amazon.com/images/I/71U2q4xJsHL._AC_UX679_.jpg');
+INSERT INTO product VALUES('024', 'K-Hoodie', 8.99, '10','3', 'L','Yellow','Silk','Quality made Hoodie', 'https://m.media-amazon.com/images/I/71nUJDY6MML._AC_UY879_.jpg');
+
+INSERT INTO product VALUES('031', 'K-Jeans', 9.99, '10','0', 'XL','Black','Denim','Quality made jeans', 'https://m.media-amazon.com/images/I/81CJhBHAEIL._AC_UY879_.jpg');
+INSERT INTO product VALUES('032', 'K-Cargo Pants', 9.99, '10','1', 'L','Light Blue','Cotton','Quality made Cargo Pants', 'https://m.media-amazon.com/images/I/616DSwOWFgL._AC_UX679_.jpg');
+INSERT INTO product VALUES('033', 'K-Dress Pants', 9.99, '10','2', 'M','Brown','Silk','Quality made Dress Pants', 'https://m.media-amazon.com/images/I/71n5iU5mwkL._AC_UY879_.jpg');
+INSERT INTO product VALUES('034', 'K-Joggers', 9.99, '10','3', 'S','Dark Blue','Cotton','Quality made Joggers', 'https://m.media-amazon.com/images/I/61h2cjRTlcL._AC_UX679_.jpg');
+
+INSERT INTO payment VALUES('678-232-1230', '420','2025-12-05','John', 'Doe', '241', 'FDR ST', 'Apt 4f', 'New York City', 'NY', '11203');
+INSERT INTO payment VALUES('678-222-1130', '410','2025-11-11','Juan', 'Quando', '321', 'GWB ST', 'Apt 1f', 'Brooklyn', 'NY', '11235');
+INSERT INTO payment VALUES('678-232-1120', '430','2025-12-20','Villados', 'Cronos', '322', 'OHB ST', 'Apt 6f', 'Kent', 'OH', '44232');
