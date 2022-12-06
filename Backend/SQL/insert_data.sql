abe86ff2fbbfc844294d41b11baf89a9af3b84f0
Alex Ely <ely.m.alexander@gmail.com>
2022-12-05 22:26:45 -0500

    Added all inserts

--- Backend/SQL/insert_data.sql
+++ Backend/SQL/insert_data.sql
@@ -22,6 +22,30 @@ INSERT INTO product VALUES('032', 'K-Cargo Pants', 9.99, '10','1', 'L','Light Bl
 INSERT INTO product VALUES('033', 'K-Dress Pants', 9.99, '10','2', 'M','Brown','Silk','Quality made Dress Pants', 'https://m.media-amazon.com/images/I/71n5iU5mwkL._AC_UY879_.jpg');
 INSERT INTO product VALUES('034', 'K-Joggers', 9.99, '10','3', 'S','Dark Blue','Cotton','Quality made Joggers', 'https://m.media-amazon.com/images/I/61h2cjRTlcL._AC_UX679_.jpg');
 
+INSERT INTO product VALUES('041', 'K-Shoes', 9.99, '10','0', 'XL','Black','Cotton','Quality made shoes', 'https://m.media-amazon.com/images/I/81cuTtP+n6L._AC_UL1500_.jpg');
+INSERT INTO product VALUES('042', 'K-Boots', 9.99, '10','1', 'L','Light Blue','Leather','Quality made Boots', 'https://m.media-amazon.com/images/I/71Zg-TNizyL._AC_UL1280_.jpg');
+INSERT INTO product VALUES('043', 'K-Slippers', 9.99, '10','2', 'M','Brown','Rubber','Quality made Slippers', 'https://m.media-amazon.com/images/I/510XDBuRdLL._AC_UL1000_.jpg');
+INSERT INTO product VALUES('044', 'K-Overcoat', 9.99, '10','3', 'S','Dark Blue','Cotton','Quality made Overcoat', 'https://m.media-amazon.com/images/I/51UOE7eiCRL._AC_UL1002_.jpg');
+INSERT INTO product VALUES('045', 'K-Backpack', 9.99, '10','3', 'S','Dark Blue','Cotton','Quality made Backpack', 'https://m.media-amazon.com/images/I/81OFxhFWmML._AC_SX466_.jpg');
+
+INSERT INTO product VALUES('051', 'K-Puffer Jacket', 9.99, '10','0', 'XL','Black','Cotton','Quality made Puffer Jackets', 'https://m.media-amazon.com/images/I/71M7trPkIZL._AC_UL1500_.jpg');
+INSERT INTO product VALUES('052', 'K-Guci Flips', 9.99, '10','1', 'L','Light Blue','Cotton','Quality made Flips', 'https://m.media-amazon.com/images/I/71iHyRq0uHL._AC_SX679_.jpg');
+INSERT INTO product VALUES('053', 'K-Gloves', 9.99, '10','2', 'M','Brown','Silk','Quality made Dress Gloves', 'https://m.media-amazon.com/images/I/91ucHu48VEL._AC_UL1500_.jpg');
+INSERT INTO product VALUES('054', 'K-Helmet', 9.99, '10','3', 'S','Dark Blue','Plastic & Metal','Quality made Helmet', 'https://m.media-amazon.com/images/I/51hMQvVAlpL._AC_SY450_.jpg');
+INSERT INTO product VALUES('055', 'K-Wristband', 9.99, '10','3', 'S','Dark Blue','Rubber','Quality made wristbands', 'https://m.media-amazon.com/images/I/81YHlY0KX6L._AC_SX679_.jpg');
+
+INSERT INTO product VALUES('061', 'Dio Jacket', 119.99, '10','0', 'XL','Black','Dio','Its Dio', 'https://m.media-amazon.com/images/I/61FvimThCpL._AC_UL1500_.jpg');
+INSERT INTO product VALUES('062', 'Short Cargo', 39.99, '10','1', 'L','Light Blue','Cotton','Quality made Short Cargo Pants', 'https://m.media-amazon.com/images/I/61fKSaYioyL._AC_UL1001_.jpg');
+INSERT INTO product VALUES('063', 'K-Dress Jacket',22.99, '10','2', 'M','Brown','Silk','Quality made Dress Jacket', 'https://m.media-amazon.com/images/I/51kE+LjHirL._AC_UL1500_.jpg');
+INSERT INTO product VALUES('064', 'K-Leather Jacket', 33.99, '10','3', 'S','Dark Blue','Leather','Quality made Leather Jacket', 'https://m.media-amazon.com/images/I/7191fRCs43L._AC_UL1500_.jpg');
+INSERT INTO product VALUES('065', 'K-Sweater', 6.99, '10','3', 'S','Dark Blue','Cotton','Quality made Sweater', 'https://m.media-amazon.com/images/I/81720hdnA2L._AC_UL1500_.jpg');
+
+INSERT INTO product VALUES('0312', 'Dio Jacket', 119.99, '10','1', 'XL','Black','Dio','Its Dio', 'https://m.media-amazon.com/images/I/61FvimThCpL._AC_UL1500_.jpg');
+INSERT INTO product VALUES('0322', 'Short Cargo', 39.99, '10','1', 'L','Light Blue','Cotton','Quality made Short Cargo Pants', 'https://m.media-amazon.com/images/I/61fKSaYioyL._AC_UL1001_.jpg');
+INSERT INTO product VALUES('0332', 'K-Dress Jacket',22.99, '10','2', 'M','Brown','Silk','Quality made Dress Jacket', 'https://m.media-amazon.com/images/I/51kE+LjHirL._AC_UL1500_.jpg');
+INSERT INTO product VALUES('0342', 'K-Leather Jacket', 33.99, '10','2', 'S','Dark Blue','Leather','Quality made Leather Jacket', 'https://m.media-amazon.com/images/I/7191fRCs43L._AC_UL1500_.jpg');
+INSERT INTO product VALUES('0343' ,'K-Sweater', 6.99, '10','0', 'S','Dark Blue','Cotton','Quality made Sweater', 'https://m.media-amazon.com/images/I/81720hdnA2L._AC_UL1500_.jpg');
+
 INSERT INTO payment VALUES('678-232-1230', '420','2025-12-05','John', 'Doe', '241', 'FDR ST', 'Apt 4f', 'New York City', 'NY', '11203');
 INSERT INTO payment VALUES('678-222-1130', '410','2025-11-11','Juan', 'Quando', '321', 'GWB ST', 'Apt 1f', 'Brooklyn', 'NY', '11235');
 INSERT INTO payment VALUES('678-232-1120', '430','2025-12-20','Villados', 'Cronos', '322', 'OHB ST', 'Apt 6f', 'Kent', 'OH', '44232');
